d2271554dc3d0178fbb504f3ad2f1a5a74709026
Gabriele Lucci <gabriele@lucci.xyz>
2020-01-02 17:34:57 +0100

    Fix demo settings prefix

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -106,31 +106,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = "/static/"
-
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "static"))
 
 # Stored files
 # https://docs.djangoproject.com/en/2.2/topics/files/
-
 MEDIA_URL = "/media/"
-
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, "media"))
 
-TASK_MANAGER_N_REPORTS_INLINE = 5
-TASK_MANAGER_N_LINES_IN_REPORT_INLINE = 10
-TASK_MANAGER_SHOW_LOGVIEWER_LINK = True
-TASK_MANAGER_USE_FILTER_COLLAPSE = True
+UWSGI_TASKMANAGER_N_REPORTS_INLINE = 5
+UWSGI_TASKMANAGER_N_LINES_IN_REPORT_INLINE = 10
+UWSGI_TASKMANAGER_SHOW_LOGVIEWER_LINK = True
+UWSGI_TASKMANAGER_USE_FILTER_COLLAPSE = True
