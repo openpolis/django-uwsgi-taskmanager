f9042157f62bbdbeed1870110951ee754a397c5f
Gabriele Lucci <gabriele@lucci.xyz>
2020-01-02 17:36:11 +0100

    utils: Add "get_base_url" function

--- taskmanager/utils.py
+++ taskmanager/utils.py
@@ -1,4 +1,9 @@
 """Define utils for the taskmanager app."""
+from typing import Optional
+
+from django.apps import apps
+
+from taskmanager.settings import UWSGI_TASKMANAGER_BASE_URL
 
 
 def log_tail(log, n_lines=10):
@@ -8,3 +13,15 @@ def log_tail(log, n_lines=10):
     if hidden_lines > 0:
         lines = [f"{hidden_lines} lines hidden ..."] + lines[hidden_lines:]
     return "\n".join(lines)
+
+
+def get_base_url() -> Optional[str]:
+    try:
+        # Check if "sites" framework is available; return current site domain.
+        if apps.get_app_config("sites"):
+            from django.contrib.sites.models import Site
+            return Site.objects.get_current().domain
+    except (LookupError, ImportError):
+        pass
+    # Fallback to UWSGI_TASKMANAGER_BASE_URL setting value; strip protocol bit
+    return UWSGI_TASKMANAGER_BASE_URL.strip("https://").strip("http://")
