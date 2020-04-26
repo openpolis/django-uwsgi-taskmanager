502beb854f1825f5603cc7699669be3a82546e50
Gabriele Lucci <gabriele@lucci.xyz>
2020-04-26 08:55:42 +0200

    models: Temporarily disable notifications feature

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,27 +1,22 @@
 """Define Django models for the taskmanager app."""
 
 import datetime
+from io import StringIO
 import os
 import re
-from io import StringIO
 
-from django.core.mail import send_mail
 from django.core.management import load_command_class
 from django.db import models
-from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from taskmanager.settings import (
     UWSGI_TASKMANAGER_N_REPORTS_INLINE,
     UWSGI_TASKMANAGER_NOTIFICATIONS_EMAIL_FROM,
     UWSGI_TASKMANAGER_NOTIFICATIONS_EMAIL_RECIPIENTS,
-    UWSGI_TASKMANAGER_NOTIFICATIONS_FAILURE_MESSAGE,
     UWSGI_TASKMANAGER_NOTIFICATIONS_SLACK_CHANNELS,
     UWSGI_TASKMANAGER_NOTIFICATIONS_SLACK_TOKEN,
-    UWSGI_TASKMANAGER_NOTIFICATIONS_WARNINGS_MESSAGE,
 )
 from taskmanager.tasks import exec_command_task
-from taskmanager.utils import get_base_url, log_tail
 
 try:
     import slack  # noqa
@@ -121,104 +116,9 @@ class Report(models.Model):
                 ]
             )
             if slack_configured:
-                slack_client = slack.WebClient(
-                    token=UWSGI_TASKMANAGER_NOTIFICATIONS_SLACK_TOKEN
-                )
-                blocks = [
-                    {
-                        "type": "context",
-                        "elements": [
-                            {"type": "mrkdwn", "text": f"django-uwsgi-taskmanager"}
-                        ],
-                    }
-                ]
-                if self.invocation_result == self.RESULT_FAILED:
-                    blocks.append(
-                        {
-                            "type": "section",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": UWSGI_TASKMANAGER_NOTIFICATIONS_FAILURE_MESSAGE.format(
-                                    task_name=self.task.name,
-                                    invocation_time=(
-                                        "<!date"
-                                        f"^{int(self.invocation_datetime.timestamp())}"
-                                        "^{date_num} {time_secs}|???>"
-                                    ),
-                                ),
-                            },
-                        }
-                    )
-                else:
-                    blocks.append(
-                        {
-                            "type": "section",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": UWSGI_TASKMANAGER_NOTIFICATIONS_WARNINGS_MESSAGE.format(
-                                    task_name=self.task.name,
-                                    invocation_time=(
-                                        "<!date"
-                                        f"^{int(self.invocation_datetime.timestamp())}"
-                                        "^{date_num} {time_secs}|???>"
-                                    ),
-                                    n_errors=self.n_log_errors,
-                                    n_warnings=self.n_log_warnings,
-                                ),
-                            },
-                        }
-                    )
-                blocks.append(
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": f"Logs tail:\n ```{log_tail(self.log)}```",
-                        },
-                    }
-                )
-                base_url = get_base_url()
-                if base_url:
-                    logviewer_url = reverse("log_viewer", args=(self.id,))
-                    blocks.append(
-                        {
-                            "type": "context",
-                            "elements": [
-                                {
-                                    "type": "mrkdwn",
-                                    "text": f"<http://{base_url}{logviewer_url}|Full logs>",
-                                }
-                            ],
-                        }
-                    )
-                for channel in UWSGI_TASKMANAGER_NOTIFICATIONS_SLACK_CHANNELS:
-                    slack_client.chat_postMessage(channel=channel, blocks=blocks)
-                    # Fail silently
+                pass
             if email_configured:
-                if self.invocation_result == self.RESULT_FAILED:
-                    subject = f"{self.task.name} failed"
-                    message = f"An error occurred: "
-                else:
-                    subject = f"{self.task.name} completed with warning"
-                    message = (
-                        f"{self.task.name}, "
-                        f"invoked at {self.invocation_datetime}, "
-                        f"completed with {self.n_log_warnings} warnings "
-                        f"and {self.n_log_errors} errors."
-                    )
-                send_mail(
-                    subject=subject,
-                    message=message,
-                    from_email=UWSGI_TASKMANAGER_NOTIFICATIONS_EMAIL_FROM,
-                    recipient_list=UWSGI_TASKMANAGER_NOTIFICATIONS_EMAIL_RECIPIENTS,
-                    fail_silently=True,
-                )
-
-    class Meta:
-        """Django model options."""
-
-        verbose_name = _("Report")
-        verbose_name_plural = _("Reports")
+                pass
 
 
 class TaskCategory(models.Model):
