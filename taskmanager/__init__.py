78500fc27904f53a5a59821dea0189394a5c4e6d
guglielmo <guglielmo@openpolis.it>
2021-04-10 11:33:05 +0200

    Bump version: 2.2.9 → 2.2.10

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,7 +1,7 @@
 """Django application to manage async tasks via admin interface, using uWSGI spooler."""
 
 # PEP 440 - version number format
-VERSION = (2, 2, 9)
+VERSION = (2, 2, 10)
 
 # PEP 396 - module version variable
 __version__ = ".".join(map(str, VERSION))
